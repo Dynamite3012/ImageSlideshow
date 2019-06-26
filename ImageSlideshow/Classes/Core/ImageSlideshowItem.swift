ace28b46103f100a5b2d7cc14e81558a07d0275b
Денис Демидов <ddemiov@gmail.com>
2019-06-26 13:33:59 +0300

    temp5

--- ImageSlideshow/Classes/Core/ImageSlideshowItem.swift
+++ ImageSlideshow/Classes/Core/ImageSlideshowItem.swift
@@ -231,9 +231,7 @@ open class ImageSlideshowItem: UIScrollView, UIScrollViewDelegate {
     }
 
     open func scrollViewWillBeginZooming(_ scrollView: UIScrollView, with view: UIView?) {
-        if isZoomed() {
-            zoomingDelegate?.beginZooming()
-        }
+        zoomingDelegate?.beginZooming()
     }
 
     open func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
